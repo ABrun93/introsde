83d6b7637873af0f27a73396e5f0822f1ad09e91
ABrun93 <a.brun93@gmail.com>
2016-10-25 23:11:15 +0200

    Add jersey_HelloWorld

---
+++ lab05/myworkspace/jersey_HelloWorld/src/jersey/HelloWorld.java
@@ -0,0 +1,49 @@
+// Service call: http://localhost:8080/jersey_HelloWorld/jersey/salutation/
+package jersey;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/salutation") // indicates the path under which this resource will be available
+public class HelloWorld 
+{
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHelloHtml() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+                + "<body><h1>" + "Hello World in REST" + "</body></h1>"
+                + "</html> ";
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayHelloXML() {
+        return "<?xml version=\"1.0\"?>" + "<msg>" + "Hello World in REST"
+                + "</msg>";
+    }
+    
+    // When client wants HTML: http://localhost:8080/jersey_HelloWorld/jersey/salutation/Cristhian%20Parra?age=35
+    @GET
+    @Path("/{name}") // you can pass path params to a service
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHelloHtmlToPerson(@PathParam("name") String name,
+            @QueryParam("age") int age) {
+
+        String printAlsoAge = "";
+
+        // QueryParams can be used as optional parameters that you use if they are present
+        // 
+        if (age > 0) {
+            printAlsoAge = " You are "+age+ "years old, but don't worry. It's never late to learn!";
+        }
+
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+                + "<body><h1>" + "Hello "+name+". Welcome to Jersey REST." +printAlsoAge+ "</body></h1>"
+                + "</html> ";
+    } 
+}
+
